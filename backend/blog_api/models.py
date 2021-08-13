e47d62265d8a6b00b5a75e2797dd0d4b86b18450
grkm <garslan13@gmail.com>
2021-08-13 23:21:59 +0300

    Created Post schema

--- backend/blog_api/models.py
+++ backend/blog_api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
+#from django.conf import settings
+from .managers import PublishedManager
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='posts')
+    tag = models.ForeignKey(Tag, on_delete=models.PROTECT, null=True, blank=True)
+    title = models.CharField(max_length=255)
+    body = models.TextField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=127, unique_for_date='published_at')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    # Default Manager
+    objects = models.Manager()
+    # Custom Manager
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ('-published_at',)
+
+    def __str__(self):
+        return self.title
