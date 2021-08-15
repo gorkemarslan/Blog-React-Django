83184ae2b4b1e985175ce13befd5cd1ad12dda23
grkm <garslan13@gmail.com>
2021-08-15 18:59:14 +0300

    Added FontAwesomeIcon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
   return (
@@ -23,12 +25,12 @@ function Header() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link className="nav-link" exact to="/">
+              <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" exact to="/posts">
+              <NavLink className="nav-link" to="/posts">
                 Posts
               </NavLink>
             </li>
@@ -45,19 +47,32 @@ function Header() {
                 Dropdown
               </NavLink>
               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <Link className="dropdown-item" exact to="#">
+                <Link className="dropdown-item" to="#">
                   Action
                 </Link>
-                <Link className="dropdown-item" exact to="#">
+                <Link className="dropdown-item" to="#">
                   Another action
                 </Link>
                 <div className="dropdown-divider"></div>
-                <Link className="dropdown-item" exact to="#">
+                <Link className="dropdown-item" to="#">
                   Something else here
                 </Link>
               </div>
             </li>
           </ul>
+          <ul className="nav navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">
+                <FontAwesomeIcon icon={faUser} /> Sign Up
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                <FontAwesomeIcon icon={faSignInAlt} /> Login
+              </Link>
+            </li>
+          </ul>
+         
         </div>
       </nav>
     </div>
