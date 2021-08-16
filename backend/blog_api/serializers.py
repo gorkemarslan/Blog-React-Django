79e03e1dc55ebff19854f83db6b06ec9860f4b93
grkm <garslan13@gmail.com>
2021-08-16 23:35:34 +0300

    Updated serializers.py

--- backend/blog_api/serializers.py
+++ backend/blog_api/serializers.py
@@ -3,7 +3,10 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source='author.email', read_only=True)
+    tag = serializers.CharField(source='tag.name', read_only=True)
+
     class Meta:
         model = Post
-        fields = ('id', 'title', 'body', 'tag', 'status', 'published_at', 'slug')
+        fields = ('id', 'title', 'body', 'author', 'tag', 'status', 'published_at', 'slug')
         lookup_field = 'slug'
